9ac1d1ef990d38c6fc1f1955ed04731c7de01764
Vihaan0412 <vihaangandhi2102@gmail.com>
2026-07-07 17:42:30 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,11 @@
+def well_wishes(name):
+    print("Hello", name)
+    print("how are you? \n")
+
+well_wishes("Prachi")
+well_wishes("Smera")
+well_wishes("Vihaan")
+name = str(input("What is your name:"))
+print("Hello", name)
+print("how are you? \n")
+
---
+++ a2.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print('The weather is pleasant in', spring)
+    print("THe weather is same in", autumn)
+
+spring = "autumn"
+autumn = "spring"
+weather_condition()
